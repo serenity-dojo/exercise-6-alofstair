289a7c82ff833fd87d050aace14282a2863142a2
agoodall <agoodall@workforcesoftware.com>
2023-06-26 15:48:11 +0100

    try 1

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -1,7 +1,15 @@
 package com.serenitydojo.model;
 
 public class Feeder {
+
     public String feeds(String animal, boolean isPremium) {
-        return "Food";
+        if (animal.equals("Hamster")) {
+            return isPremium ? "Lettuce" : "Cabbage";
+        } else if (animal.equals("Dog")) {
+            return isPremium ? "DeluxeDogFood" : "Dog Food";
+        } else if (animal.equals("Cat")) {
+            return isPremium ? "Salmon" : "Tuna";
+        }
+        return animal;
     }
 }
